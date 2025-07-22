95f4b49e4b6f803116181044ac7ba27ab829f39e
c4ller8 <callumobrien444@gmail.com>
2025-07-22 19:01:26 +0100

    added code institute tutorial

--- my_project/settings.py
+++ my_project/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-cj_^yx6@q_v%suz_b@#j_m333q7va=!8x&^pq6bl&!+xqm-d&e'
+SECRET_KEY= 
+'django-insecure-cj_^yx6@q_v%suz_b@#j_m333q7va=!8x&^pq6bl&!+xqm-d&e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
