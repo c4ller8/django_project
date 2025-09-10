fb7f06b39ae2d8ebf0bb8ade229a7e9eeedbec0b
c4ller8 <callumobrien444@gmail.com>
2025-09-10 17:22:41 +0100

    Add view name list to episodes urlpatterns.

--- episodes/urls.py
+++ episodes/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'episodes'  # This sets the application namespace
 
 urlpatterns = [
-    path('', views.episodes, name='episodes'),
+    path('', views.episodes, name='list'),
 ]
