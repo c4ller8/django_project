f3f957e5701505225b51285ca47f253ae28759b0
c4ller8 <callumobrien444@gmail.com>
2025-09-19 11:28:31 +0100

    try to get cloudinary  api working after fatal error.

--- my_project/settings.py
+++ my_project/settings.py
@@ -15,6 +15,19 @@ from pathlib import Path
 import dj_database_url
 if os.path.exists("env.py"):
     import env  # noqa
+import cloudinary  # ← Add these at top with other imports
+import cloudinary.uploader
+import cloudinary.api
+import cloudinary
+from dotenv import load_dotenv
+load_dotenv()
+
+cloudinary.config(
+    cloud_name=os.environ.get('CLOUDINARY_CLOUD_NAME'),
+    api_key=os.environ.get('CLOUDINARY_API_KEY'),
+    api_secret=os.environ.get('CLOUDINARY_API_SECRET')
+)
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
